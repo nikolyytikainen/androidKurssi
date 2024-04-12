d62fa5a2161199be498b7671be760fa93f170a82
nikolyytikainen <nlyytikainen@gmail.com>
2024-04-12 11:38:40 +0300

    ajastinta muokattu

--- app/src/main/java/com/example/androidkurssi/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/androidkurssi/ui/notifications/NotificationsFragment.java
@@ -1,11 +1,18 @@
 package com.example.androidkurssi.ui.notifications;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.NumberPicker;
 import android.widget.TextClock;
 import android.widget.TextView;
@@ -26,6 +33,12 @@ public class NotificationsFragment extends Fragment {
     private String[] displayedValues;
     private MaterialButtonToggleGroup buttonToggleGroup;
     private TextView countTime;
+    private CountDownTimer cTimer;
+    int time;
+    boolean paused = false;
+    long timeLeft;
+    Animation animation;
+    Ringtone currentRingtone;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -38,6 +51,10 @@ public class NotificationsFragment extends Fragment {
 
         //final TextView textView = binding.textNotifications;
         //notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        animation = AnimationUtils.loadAnimation(getContext(), R.anim.animation2);
+        Uri currentRintoneUri = RingtoneManager.getActualDefaultRingtoneUri(getActivity()
+                .getApplicationContext(), RingtoneManager.TYPE_ALARM);
+        currentRingtone = RingtoneManager.getRingtone(getActivity(), currentRintoneUri);
 
         picker = root.findViewById(R.id.numberpicker1);
         picker.setMaxValue(60);
@@ -58,26 +75,19 @@ public class NotificationsFragment extends Fragment {
 
         buttonToggleGroup = root.findViewById(R.id.toggleButton);
         countTime = root.findViewById(R.id.countTime);
+        buttonToggleGroup.findViewById(R.id.button_pause).setEnabled(false);
         buttonToggleGroup.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
             @Override public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
                 if (isChecked) {
                     if (checkedId == R.id.button_start) {
                         // start timing here
-                        int time = Integer.valueOf(picker.getValue()*1000);
-                        new CountDownTimer(time, 1000){
-
-                            public void onTick(long millisUntilFinished) {
-                                countTime.setText("seconds remaining: " + millisUntilFinished / 1000);
-                            }
-
-                            public void onFinish() {
-                                countTime.setText("done!");
-                            }
-                        }.start();
+                        startTiming();
                     } else if (checkedId == R.id.button_pause){
                         // pause timing here
+                        pauseTiming();
                     } else if (checkedId == R.id.button_stop) {
                         //stop timing here
+                        stopTiming();
                     }
                 }
             }
@@ -90,6 +100,81 @@ public class NotificationsFragment extends Fragment {
 
         return root;
     }
+
+    public void startTiming(){
+
+        buttonToggleGroup.findViewById(R.id.button_start).setEnabled(false);
+        buttonToggleGroup.findViewById(R.id.button_pause).setEnabled(true);
+
+        time = Integer.valueOf(picker.getValue()*1000+1000);
+        cTimer = new CountDownTimer (time, 1000){
+
+            public void onTick(long millisUntilFinished) {
+                countTime.setText((millisUntilFinished / 1000) + " s" );
+                timeLeft = millisUntilFinished;
+                //pause = false;
+            }
+
+            public void onFinish() {
+                countTime.setText("done!");
+                countTime.startAnimation(animation);
+                currentRingtone.play();
+                //buttonToggleGroup.findViewById(R.id.button_start).setEnabled(true);
+
+            }
+        }.start();
+
+    }
+
+    public void pauseTiming(){
+
+        paused = !paused;
+        //paused = true;
+        Log.d("pause",Boolean.toString(paused));
+        if (paused) {
+            Log.d("pauseTiming", "Paused is true");
+            if (cTimer != null) {
+                cTimer.cancel();
+                Log.d("pauseTiming", "CountDownTimer is cancelled");
+            } else {
+                Log.d("pauseTiming", "CountDownTimer is null");
+            }
+        } else {
+            Log.d("pauseTiming", "Paused is false");
+            cTimer = new CountDownTimer (timeLeft, 1000){
+
+                public void onTick(long millisUntilFinished) {
+                    countTime.setText((millisUntilFinished / 1000) + " s" );
+                    timeLeft = millisUntilFinished;
+                    //pause = false;
+                }
+
+                public void onFinish() {
+                    countTime.setText("done!");
+                    countTime.startAnimation(animation);
+                    currentRingtone.play();
+                    //buttonToggleGroup.findViewById(R.id.button_start).setEnabled(true);
+                }
+            }.start();
+
+        }
+
+
+    }
+
+    public void stopTiming(){
+        cTimer.cancel();
+        currentRingtone.stop();
+        countTime.setText("-");
+        countTime.clearAnimation();
+        buttonToggleGroup.findViewById(R.id.button_start).setEnabled(true);
+        buttonToggleGroup.findViewById(R.id.button_pause).setEnabled(false);
+
+    }
+
+
+
+
 
     @Override
     public void onDestroyView() {
